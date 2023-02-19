6ed55a172d1c92440f9faf04c900bb1172e53f9e
Leandro Martins <124270630+lehannK@users.noreply.github.com>
2023-02-19 13:06:40 -0300

    Update Dewpoint.C

--- Dewpoint.C
+++ Dewpoint.C
@@ -1,14 +1,14 @@
-'CALCULO DEWPOINT 
+// CALCULO DEWPOINT 
 
 Numeric Input Temp, RH
 Numeric Output Dewpoint
 Numeric X, Y
 
-IF Temp = null ! RH = null then     // se o server perder a referencia dos sensores, não fazer nada
-                                    // condição necessária para manter o script rodando sempre
+IF Temp = null ! RH = null then     
+
+// condição necessária para manter o script em loop
 
 Else
-
 	X = ln(RH/100)
 	X = X + ((17.625 * Temp) / (243.04 + Temp))
 	X = X * 243.04 
